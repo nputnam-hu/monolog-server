8c1d96a0322719208f4d3e7d2cff81be937c58dd
Noah Putnam <noahputnam@gmail.com>
2024-08-02 04:49:45 -0400

    update

--- src/config/database.mjs
+++ src/config/database.mjs
@@ -14,6 +14,6 @@ export default {
   production: {
     dialect: "postgres",
     databaseURL: process.env.DATABASE_URL,
-    host: "127.0.0.1",
+    host: "https://monolog-server-bc1532d12299.herokuapp.com",
   },
 };
