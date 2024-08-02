05b33bd8d453896bf84971fd1514f7c10975e848
Noah Putnam <noahputnam@gmail.com>
2024-08-02 17:14:41 -0400

    update

--- src/services/sequelize.service.mjs
+++ src/services/sequelize.service.mjs
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 import databaseConfig from "../config/database.mjs";
-import fs from "fs";
 import path from "path";
+import fs from "fs";
 const __dirname = path.resolve();
 
 const modelFiles = fs
